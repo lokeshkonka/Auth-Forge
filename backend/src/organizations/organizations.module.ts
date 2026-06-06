60f1227aa6ab81b8d643417b5d6443cb1cc58e43
lokeshkonka <konkalokesh372@gmail.com>
2026-06-06 16:59:33 +0530

    update organization module

--- backend/src/organizations/organizations.module.ts
+++ backend/src/organizations/organizations.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MembersModule } from '../members/members.module';
+import { AuditModule } from '../audit/audit.module';
 import { OrganizationsController } from './organizations.controller';
 import { OrganizationsService } from './organizations.service';
 
 @Module({
-  imports: [MembersModule],
+  imports: [MembersModule, AuditModule],
   controllers: [OrganizationsController],
   providers: [OrganizationsService],
 })
