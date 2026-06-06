5af8ba48c90ada42a48e33aac66e494fd0143589
lokeshkonka <konkalokesh372@gmail.com>
2026-06-06 16:59:33 +0530

    update roles module

--- backend/src/roles/roles.module.ts
+++ backend/src/roles/roles.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesController } from './controllers/roles.controller';
 import { PrismaModule } from '../database/prisma.module';
+import { AuditModule } from '../audit/audit.module';
+import { MembersModule } from '../members/members.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuditModule, MembersModule],
   controllers: [RolesController],
   providers: [RolesService],
   exports: [RolesService],
